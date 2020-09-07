7207a2e0ff9e3953af5dc7f054ec0e7a4633bc97
Hannes Braun <hannesbraun@mail.de>
2020-09-07 15:49:58 +0200

    Add Lillia and Yone
    New League of Legends champions
    Also update version number to 3.1.5

--- src/configuration.py
+++ src/configuration.py
@@ -1,6 +1,6 @@
 class Configuration:
     def __init__(self, db_connection_wrapper):
-        self.bot_version = "3.1.4"
+        self.bot_version = "3.1.5"
 
         cursor = db_connection_wrapper.connection.cursor()
 
